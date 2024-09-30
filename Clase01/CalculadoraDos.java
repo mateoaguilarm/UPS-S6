1b7a75ea530fa38f2dd2603a740ad69545b7eecb
Mateo <mateoaguilarm@Mateos-MacBook-Pro.local>
2024-09-30 08:10:31 -0500

    correción de double num1 a num

--- Clase01/CalculadoraDos.java
+++ Clase01/CalculadoraDos.java
@@ -5,12 +5,12 @@ public class CalculadoraDos {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingrese el numero de su tabla de multiplicar: ");
-        double num1 = scanner.nextDouble();
+        double num = scanner.nextDouble();
 
-        System.out.println("Tabla de multiplicar del " + num1 + ":");
+        System.out.println("Tabla de multiplicar del " + num + ":");
         for (int i = 1; i <= 10; i ++) { // Manteniendo la secuencia de pares
-            double resultado = num1 * i;
-            System.out.println(num1 + " x " + i + " = " + resultado);
+            double resultado = num * i;
+            System.out.println(num + " x " + i + " = " + resultado);
         }
 
         scanner.close(); // Cerramos el scanner al final
