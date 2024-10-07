83f0619f9de1b2437d29b78ffdc5c90d287f0868
mateoaguilarm <mateonaguilarm@gmail.com>
2024-10-07 09:47:05 -0500

    fixed

--- Clase01/TablaMultiplicarUno.java
+++ Clase01/TablaMultiplicarUno.java
@@ -10,7 +10,7 @@ public class TablaMultiplicarUno {
         System.out.println("Tabla de multiplicar del " + numero + ":");
         for (int i = 2; i <= 10; i+=2) {
             int resultado = numero * i;
-            System.out.println(numero + " x " + i + " = " + resu
+            System.out.println(numero + " x " + i + " = " + resultado);
 for (int i = 2; i <= 10; i+=2) {
             int resultado = numero * i;
             System.out.println(numero + " x " + i + " = " + resultado);
