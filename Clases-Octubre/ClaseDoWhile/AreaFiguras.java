686c56c8ee3c1d1ab97396404fa670f396681e7a
mateoaguilarm <mateonaguilarm@gmail.com>
2024-10-08 09:32:42 -0500

    init clase DoWhile

---
+++ Clases-Octubre/ClaseDoWhile/AreaFiguras.java
@@ -0,0 +1,11 @@
+package ClaseDoWhile;
+import java.util.Scanner;
+
+public class AreaFiguras {
+    public static void main(String[] args) {
+        System.out.println("Elige una Figura");
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
